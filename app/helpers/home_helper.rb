e8600a1fa010dcc4438dba538a70461c73dd0623
Micah Rothenbuhler <micahrothebuhler@gmail.com>
2020-11-18 16:40:57 -0800

    Finish up MVP

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,20 +1,22 @@
 require 'uri'
 require 'open-uri'
+require 'nokogiri'
 
 module HomeHelper
   def self.open_graph_data(url)
     return { error: 'invalid url' } unless is_valid_url(url)
-    file = open(url)
-    contents = file.read
-    # puts file.hash
-    # puts file.methods
-    puts ''
-    puts ''
-    # puts contents.class
-    # puts contents.methods
-    puts contents
-    puts contents.class
-    { error: 'suck sess' }
+    data = {}
+    page = Nokogiri::HTML(URI.open(url))
+    page.css('meta').each do |tag|
+      property = tag.attribute('property')
+      if property.present? && property.value.include?('og:')
+        data[property.value] = tag.attribute('content').value
+      end
+    end
+    if data.empty?
+      data[:error] = 'No opengraph data available for that URL. The site has either not set it up, or it blocks robots such as myself.'
+    end
+    data
   end
 
   private
--- test/helpers/home_helper_test.rb
+++ test/helpers/home_helper_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class HomeHelperTest < ActionDispatch::IntegrationTest
-  test 'the truth' do
-    HomeHelper.open_graph_data('https://www.youtube.com/')
+  test 'youtube open graph data' do
+    data = HomeHelper.open_graph_data('https://www.youtube.com/')
+    assert data['og:image'].present?
+  end
+
+  test 'imgur open graph data' do
+    data = HomeHelper.open_graph_data('https://imgur.com/gallery/HSz0Lye')
+    assert data['og:image'].present?
+    assert data['og:site_name'].present?
+    assert data['og:url'].present?
+    assert data['og:image:width'].present?
+    assert data['og:image:height'].present?
+    assert data['og:type'].present?
+  end
+
+  test 'bad url' do
+    data = HomeHelper.open_graph_data('asdf')
+    assert data[:error].present?
   end
 end
